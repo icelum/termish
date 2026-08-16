fe86dc3ee0326369bf590f690fd083be80f4d8c6
liubing.icelew <icelew.2025@gmail.com>
2026-08-16 14:12:45 +0800

    HostEditScreen：引入 selectable / Role（单选交互准备）

--- composeApp/src/commonMain/kotlin/dev/mssh/ui/HostEditScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/mssh/ui/HostEditScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import dev.mssh.data.ConnectionMode
